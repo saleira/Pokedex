d1bafa623cdc50bc5c411d87251aa7836e3ab1d3
saleira <goncalo.caro@gmail.com>
2024-11-08 13:49:13 +0000

    Creation of array of objects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList.push({name: 'Bulbasaur', height: 0.7, type: 'Grass'});
+pokemonList.push({name: 'Charmander', height: 0.6, type: 'Fire'});
+pokemonList.push({name: 'Quirtle', height: 0.5, type: 'Water'});
