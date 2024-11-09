01b5aa9750482e07005382df0156cd410980ce84
saleira <goncalo.caro@gmail.com>
2024-11-09 11:37:09 +0000

    Wraped pokemonList array in an IIFE

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
-    {name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']},
-    {name: 'Venusaur', height: 2, type: ['Grass', 'Poison']},
-    {name: 'Charmander', height: 0.6, type: 'Fire'},
-    {name: 'Charmeleon', height: 1.1, type: 'Fire'},
-    {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
-    {name: 'Squirtle', height: 0.5, type: 'Water'},
-    {name: 'Wartortle', height: 1, type: 'Water'},
-    {name: 'Blastoise', height: 1.6, type: 'Water'}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+        {name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']},
+        {name: 'Venusaur', height: 2, type: ['Grass', 'Poison']},
+        {name: 'Charmander', height: 0.6, type: 'Fire'},
+        {name: 'Charmeleon', height: 1.1, type: 'Fire'},
+        {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
+        {name: 'Squirtle', height: 0.5, type: 'Water'},
+        {name: 'Wartortle', height: 1, type: 'Water'},
+        {name: 'Blastoise', height: 1.6, type: 'Water'}
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+let pokemonList = pokemonRepository.getAll();
 
 pokemonList.forEach(pokemon =>{
     document.write(`${pokemon.name} (height: ${pokemon.height} m)`);
