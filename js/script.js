497fffb127667d4cf0b384b3b97fc9efa4e920a3
Saleira <goncalo.caro@gmail.com>
2024-11-10 01:32:58 +0000

    Created event listener button click on each pokemon

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 let pokemonRepository = (function () {
     let pokemonList = [
-        {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
-        {name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']},
-        {name: 'Venusaur', height: 2, type: ['Grass', 'Poison']},
-        {name: 'Charmander', height: 0.6, type: ['Fire']},
-        {name: 'Charmeleon', height: 1.1, type: ['Fire']},
-        {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
-        {name: 'Squirtle', height: 0.5, type: ['Water']},
-        {name: 'Wartortle', height: 1, type: ['Water']},
-        {name: 'Blastoise', height: 1.6, type: ['Water']}
+        {id: 1, name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+        {id: 2, name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']},
+        {id: 3, name: 'Venusaur', height: 2, type: ['Grass', 'Poison']},
+        {id: 4, name: 'Charmander', height: 0.6, type: ['Fire']},
+        {id: 5, name: 'Charmeleon', height: 1.1, type: ['Fire']},
+        {id: 6, name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
+        {id: 7, name: 'Squirtle', height: 0.5, type: ['Water']},
+        {id: 8, name: 'Wartortle', height: 1, type: ['Water']},
+        {id: 9, name: 'Blastoise', height: 1.6, type: ['Water']}
     ];
 
     function add(pokemon) {
@@ -19,16 +19,41 @@ let pokemonRepository = (function () {
         return pokemonList;
     }
 
+    // Function to show Pokémon details to the console
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
+    // Function to create list items with Pokémon details including a button that displays the pokemon info to the console
+    function addListItem(pokemon) {
+        let pokemonElementList = document.querySelector('.pokemon-list'); // Selects the ul with the class 'pokemon-list'
+        let listItem = document.createElement('li'); // Creates a list item
+        let button = document.createElement('button'); // Creates a button
+
+        button.innerText = pokemon.name; // Sets the button text to the Pokémon's name
+        button.classList.add('button-class'); // Adds a class to the button
+        buttonEventListener(button, pokemon); // Calls the buttonEventListener function with the button and pokemon as arguments
+
+        listItem.appendChild(button); // Appends the button to the list item
+        pokemonElementList.appendChild(listItem); // Appends the list item to the ul
+    }
+
+    // Function to add event listener to button
+    function buttonEventListener(button, pokemon) {
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        });
+    }
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem
     };
 })();
 
 let pokemonList = pokemonRepository.getAll();
 
 pokemonList.forEach(pokemon =>{
-    document.write(`${pokemon.name} (height: ${pokemon.height} m)`);
-    if (pokemon.height >= 1.5) document.write(' - Wow, that\'s big!');
-    document.write('<br>');
+    pokemonRepository.addListItem(pokemon);
 });
