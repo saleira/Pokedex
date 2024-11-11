834499105312718a013c28b0c79775dc5a91ac01
Saleira <goncalo.caro@gmail.com>
2024-11-11 17:45:57 +0000

    Updated limit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1500';
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         pokemonList.push(pokemon);
