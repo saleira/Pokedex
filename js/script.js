5b7eef8b1701e342daba1247dcd0e3ed177df459
saleira <goncalo.caro@gmail.com>
2024-11-16 20:00:16 +0000

    Added dropdown options from API's and added logic

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
-    let pokemonListBackup = [];
     let offset = 0;
     const limit = 20;
-    let apiUrlBase = 'https://pokeapi.co/api/v2/pokemon';
+    let apiAllPokemons = 'https://pokeapi.co/api/v2/pokemon';
+    let apiGeneration = 'https://pokeapi.co/api/v2/generation';
+    let apiType = 'https://pokeapi.co/api/v2/type';
     let loading = false;
+    let currentGeneration = null;
+    let currentType = null;
     const typeColors = {
-        Bug: '#a8b820',
-        Dragon: '#7038f8',
-        Grass: '#9bcc50',
-        Steel: '#b8b8d0',
-        Dark: '#707070',
-        Flying: '#a890f0',
-        Normal: '#a4acaf',
-        Ghost: '#705898',
-        Rock: '#b8a038',
-        Ground: '#e0c068',
-        Fighting: '#c02038',
-        Fire: '#f08030',
-        Electric: '#f8d030',
-        Poison: '#a040a0',
-        Psychic: '#f366b9',
-        Fairy: '#fdb9e9', 
-        Water: '#6890f0',
-        Ice: '#98d8d8'
+        bug: '#a8b820',
+        dragon: '#7038f8',
+        grass: '#9bcc50',
+        steel: '#b8b8d0',
+        dark: '#707070',
+        flying: '#a890f0',
+        normal: '#a4acaf',
+        ghost: '#705898',
+        rock: '#b8a038',
+        ground: '#e0c068',
+        fighting: '#c02038',
+        fire: '#f08030',
+        electric: '#f8d030',
+        poison: '#a040a0',
+        psychic: '#f366b9',
+        fairy: '#fdb9e9', 
+        water: '#6890f0',
+        ice: '#98d8d8'
     };
 
     function add(pokemon) {
@@ -38,10 +41,7 @@ let pokemonRepository = (function () {
         return '#' + id.toString().padStart(3, '0');
     }
 
-    function capitalizeFirstLetter(val) {
-        return String(val).charAt(0).toUpperCase() + String(val).slice(1);
-    }
-
+    // Create a card for each Pokémon
     function createCard(pokemon) {
         const card = document.querySelector('#pokemon-main-card');
         const gridElement = document.createElement('div');
@@ -58,8 +58,8 @@ let pokemonRepository = (function () {
         gridElement.appendChild(cardBody);
 
         const cardTitle = document.createElement('h5');
-        cardTitle.innerText = capitalizeFirstLetter(pokemon.name);
-        cardTitle.classList.add('mb-0');
+        cardTitle.innerText = pokemon.name;
+        cardTitle.classList.add('mb-0', 'text-capitalize');
 
         cardBody.appendChild(cardTitle);
         const cardSubtitle = document.createElement('h6');
@@ -72,72 +72,87 @@ let pokemonRepository = (function () {
         cardBody.appendChild(typesContainer);
         pokemon.types.forEach(type => {
             const pokemonType = document.createElement('p');
-            pokemonType.classList.add('g-col-6', 'text-center', 'p-2', 'rounded-3', 'm-0');
-            const typeName = capitalizeFirstLetter(type.type.name);
+            pokemonType.classList.add('g-col-6', 'text-center', 'p-2', 'rounded-3', 'm-0', 'text-capitalize');
+            const typeName = type.type.name;
             pokemonType.innerText = typeName;
             pokemonType.style.backgroundColor = typeColors[typeName];
             typesContainer.appendChild(pokemonType);
         });
     }
+    // Render the list of Pokémon to the page
+    function renderPokemonList() {
+        const cardContainer = document.querySelector('#pokemon-main-card');
+        cardContainer.innerHTML = '';
+        getAll().forEach(pokemon => createCard(pokemon));
+    }
 
+    // Load the list of Pokémon from the API and show a spinner while loading
     function loadList() {
-        if (loading) return; // Exit if already loading
-        loading = true;      // Set the flag to indicate loading in progress
+        if (loading) return;
+        loading = true;
 
-        const spinnerContainer = document.createElement('div');
-        spinnerContainer.classList.add('d-flex', 'justify-content-center', 'm-5'); 
+        showLoading();
 
-        // Spinner element
-        const spinnerElement = document.createElement('div');
-        spinnerElement.classList.add('spinner-border', 'text-primary');
-        spinnerElement.setAttribute('role', 'status');
+        let apiUrl;
+        if (currentGeneration) {
+            apiUrl = `${apiGeneration}/${currentGeneration}`;
+            const cardContainer = document.querySelector('#pokemon-main-card');
+            cardContainer.innerHTML = '';
+        } else if (currentType) {
+            apiUrl = currentType;
+            const cardContainer = document.querySelector('#pokemon-main-card');
+            cardContainer.innerHTML = '';
+        } else {
+            apiUrl = `${apiAllPokemons}?offset=${offset}&limit=${limit}`;
+            enableScrollLoading();
+        }
 
-        // Insert the spinnerContainer after #pokemon-main-card
-        spinnerContainer.appendChild(spinnerElement);
-
-        // Append the spinner container to the #pokemon-main-card div
-        const mainContainer = document.querySelector('#pokemon-main-card');
-        mainContainer.insertAdjacentElement('afterend', spinnerContainer);
-
-        let apiUrl = `${apiUrlBase}?offset=${offset}&limit=${limit}`;
-        return fetch(apiUrl)
+        fetch(apiUrl)
             .then(response => response.json())
-            .then(json => {
-                let loadPromises = json.results.map(item => {
-                    let pokemon = {
-                        name: item.name,
-                        detailsUrl: item.url
-                    };
-                    return pokemonRepository.loadDetails(pokemon).then(() => pokemon);
-                });
+            .then(async (data) => {
+                if (currentGeneration) {
+                    const speciesList = data.pokemon_species
 
-                // Wait for all details to load
-                return Promise.all(loadPromises);
-            })
-            .then(pokemonBatch => {
-                // Sort the batch by ID
-                pokemonBatch.sort((a, b) => a.id - b.id);
+                    const loadPromises = speciesList.map(async (species) => {
+                        const pokemon = {name: species.name, detailsUrl: species.url.replace('-species', '')};
+                        await loadPokemonDetails(pokemon);
+                        add(pokemon);
+                    });
+                    await Promise.all(loadPromises);
+                } else if (currentType) {
+                    const pokemonList = data.pokemon;
+                    console.log(pokemonList);
 
-                // Add each Pokémon in the sorted order
-                pokemonBatch.forEach(pokemon => {
-                    pokemonRepository.createCard(pokemon);
-                });
+                    const loadPromises = pokemonList.map(async (item) => {
+                        const pokemon = {name: item.pokemon.name, detailsUrl: item.pokemon.url};
+                        await loadPokemonDetails(pokemon);
+                        add(pokemon);
+                    });
+                    await Promise.all(loadPromises);
+                } else {
+                    const loadPromises = data.results.map(async (item) => {
+                        const pokemon = {name: item.name, detailsUrl: item.url};
+                        await loadPokemonDetails(pokemon);
+                        add(pokemon);
+                    });
+                    await Promise.all(loadPromises);
+                    offset += limit;
+                }
 
-                // Update the offset for the next batch
-                offset += limit;
+                pokemonList.sort((a, b) => a.id - b.id);
+                renderPokemonList();
             })
             .catch(error => {
-                console.error("Error loading Pokémon:", error);
+                console.error('Error loading Pokémon:', error);
             })
             .finally(() => {
-                // Remove the spinner and reset the loading flag
-                spinnerContainer.remove();
+                hideLoading();
                 loading = false;
             });
     }
 
-
-    async function loadDetails(item) {
+    // Load the details for a single Pokémon
+    async function loadPokemonDetails(item) {
         let url = item.detailsUrl;
         try {
             const response = await fetch(url);
@@ -159,21 +174,135 @@ let pokemonRepository = (function () {
         }
     }
 
+    // Fetch the list of generations or types
+    async function loadDropdowns(apiUrl) {
+        try {
+            const response = await fetch(apiUrl);
+            const details = await response.json();
+            return details.results;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    // Create a dropdown list
+    function createDropdown(list, dropdownSelector){
+        const dropdown = document.querySelector(dropdownSelector);
+        dropdown.innerHTML = '';
+        list.forEach(item => {
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
+            link.classList.add('dropdown-item', 'text-capitalize');
+            link.href = '#';
+            link.innerText = item.name;
+            listItem.appendChild(link);
+            dropdown.appendChild(listItem);
+        })
+    }
+
+    function loadGenerationPokemons(generationId) {
+        disableScrollLoading();
+        currentGeneration = generationId;
+        currentType = null;
+        pokemonList = [];
+        loadList();
+    }
+
+    function loadTypePokemons(typeUrl) {
+        disableScrollLoading();
+        currentType = typeUrl;
+        currentGeneration = null;
+        pokemonList = [];
+        loadList();
+    }
+
+    // Load the generation information for the dropdown
+    async function loadGenerationDropdown() {
+        const generationList = await loadDropdowns(apiGeneration);
+        createDropdown(generationList, '#generationDropdown');
+
+        const dropdownItems = document.querySelectorAll('#generationDropdown .dropdown-item');
+        dropdownItems.forEach((item, index) => {
+            item.addEventListener('click', () => loadGenerationPokemons(index + 1));
+        });
+    }
+
+    // Load the type information for the dropdown
+    async function loadTypeDropdown() {
+        let typeList = await loadDropdowns(apiType);
+        typeList = typeList.filter(type => type.name !== 'unknown');
+        typeList.sort((a, b) => a.name.localeCompare(b.name));
+        createDropdown(typeList, '#typeDropdown');
+
+        const dropdownItems = document.querySelectorAll('#typeDropdown .dropdown-item');
+        dropdownItems.forEach((item) => {
+            item.addEventListener('click', () => {
+                loadTypePokemons(apiType + '/' + item.innerText.toLowerCase());
+            });
+        });
+    }
+
+    function showLoading() {
+        const spinnerContainer = document.createElement('div');
+        spinnerContainer.id = 'loading-spinner'; // Assign an ID for easy removal
+        spinnerContainer.classList.add('d-flex', 'justify-content-center', 'm-5');
+
+        const spinnerElement = document.createElement('div');
+        spinnerElement.classList.add('spinner-border', 'text-primary');
+        spinnerElement.setAttribute('role', 'status');
+        spinnerContainer.appendChild(spinnerElement);
+
+        const mainContainer = document.querySelector('#pokemon-main-card');
+        mainContainer.insertAdjacentElement('afterend', spinnerContainer);
+    }
+
+    function hideLoading() {
+        const spinnerContainer = document.querySelector('#loading-spinner');
+        if (spinnerContainer) {
+            spinnerContainer.remove();
+        }
+    }
+
+    function enableScrollLoading() {
+        window.addEventListener('scroll', handleScroll);
+    }
+
+    function disableScrollLoading() {
+        window.removeEventListener('scroll', handleScroll);
+    }
+
+    function handleScroll() {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+            pokemonRepository.loadList();
+        }
+    }
+
+    function resetFilter() {
+        currentGeneration = null;
+        currentType = null;
+        offset = 0;
+        pokemonList = [];
+        loadList();
+        enableScrollLoading();
+    }
+
     return {
-        add: add,
-        getAll: getAll,
         loadList: loadList,
-        loadDetails: loadDetails,
-        createCard: createCard
+        loadPokemonDetails: loadPokemonDetails,
+        createCard: createCard,
+        loadGenerationDropdown: loadGenerationDropdown,
+        loadTypeDropdown: loadTypeDropdown,
+        resetFilter: resetFilter
     };
 })();
 
-// Initial load
-pokemonRepository.loadList();
 
-window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
-        // User is near the bottom of the page, load more Pokémon
-        pokemonRepository.loadList();
-    }
+pokemonRepository.loadList();
+pokemonRepository.loadGenerationDropdown();
+pokemonRepository.loadTypeDropdown();
+
+
+const allPokemonButtons = document.querySelectorAll('#allPokemons, #pokemonLogo');
+allPokemonButtons.forEach(button => {
+    button.addEventListener('click', pokemonRepository.resetFilter);
 });
